0cff6e56a54b8f7507eb265806489e3c161afe42
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-21 18:48:32 +0600

    "Add RegisterSerializer and LoginSerializer in accounts/serializers.py file"

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,4 +6,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model=User
         fields=['id','email']
-    
+        
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if not user:
+            raise serializers.ValidationError("Invalid credentials")
+        return user 
