40ead05429511d543498ec0ad10dff96837654a8
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-21 19:09:33 +0600

    "Add SignupView function in accounts/views.py and its url in accounts/urls.py"

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer, LoginSerializer, UserSerializer
 
-# Create your views here.
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({"message": "User created successfully"}, status=201)
+        return Response(serializer.errors, status=400)
+
