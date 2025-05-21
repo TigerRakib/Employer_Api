04fa2971176c7decf736ef90923f6c052b81e1d0
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-21 18:29:29 +0600

    "Create a serializers.py file in accounts folder and add UserSerializer class."

---
+++ accounts/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['id','email']
+    
