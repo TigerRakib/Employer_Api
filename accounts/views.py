1e7f9e1f16776a38887e8a83a215c85c85373d7b
tiger <nazrulislamrakib1813030@gmail.com>
2025-05-21 19:15:13 +0600

    "Add LoginView function in accunts/views.py and it's url in accunts/urls.py"

--- accounts/views.py
+++ accounts/views.py
@@ -12,3 +12,15 @@ class SignUpView(APIView):
             return Response({"message": "User created successfully"}, status=201)
         return Response(serializer.errors, status=400)
 
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response(serializer.errors, status=400)
+
